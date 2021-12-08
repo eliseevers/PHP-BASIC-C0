45f6865f2f1a5ecd33be11bec43695c350824ddf
eliseevers <2155620@talnet.nl>
2021-12-08 11:09:39 +0100

    opdracht 2.1 en 2.2

--- 2-Variabelen-en-datatype/2.1 - Variabelen/index.php
+++ 2-Variabelen-en-datatype/2.1 - Variabelen/index.php
@@ -2,10 +2,11 @@
 <html>
 <body>
 
-<?php
+<?php 
 $score = 0;
 echo $score;
 ?>
+
 
 </body>
 </html> 
---
+++ 2-Variabelen-en-datatype/2.2 - Datatype String/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$voornaam='Elise';
+$regelTekst1="mijn voornaam is $voornaam";
+echo $regelTekst1
+?>
+
+
+</body>
+</html> 
