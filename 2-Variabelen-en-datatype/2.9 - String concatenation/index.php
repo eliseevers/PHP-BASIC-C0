ea6733a134baa05d2dfa3cd9a714bfc818a6397e
eliseevers <2155620@talnet.nl>
2021-12-15 15:11:49 +0100

    2.9

---
+++ 2-Variabelen-en-datatype/2.9 - String concatenation/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Elise";
+$leeftijd = 16;
+echo "jouw naam is " . $voornaam1 . " en je bent " . $leeftijd . " jaar oud";
+?>
+
+</body>
+</html> 
