62750b53df1dbaeb523da526145833ed68117b43
eliseevers <2155620@talnet.nl>
2021-12-15 12:45:20 +0100

    2.8

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Elise";
+$voornaam2 = "Anne";
+$voornaam3 = "Naureen";
+echo "<ul><li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li></ul>";
+?>
+
+</body>
+</html> 
