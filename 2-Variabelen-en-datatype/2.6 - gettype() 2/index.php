e5b100a48a7de9cda07e9e27179026dfe550b0a6
eliseevers <2155620@talnet.nl>
2021-12-15 11:53:27 +0100

    2.6

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam = "Elise";
+$prijs = 0.98;
+$leeftijd = 16;
+$test = true;
+$Test = null;
+echo gettype($voornaam);
+echo gettype($prijs);
+echo gettype($leeftijd);
+echo gettype($test);
+echo gettype($Test);
+?>
+
+</body>
+</html> 
