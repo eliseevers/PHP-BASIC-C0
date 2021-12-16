4426cb0a0aa87fd7a1f1ae834b398692ba568621
eliseevers <2155620@talnet.nl>
2021-12-16 10:02:21 +0100

    3.1

---
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$mymovie = array ("<li>spiderman</li>" , "<li>batman</li>" , "<li>the joker</li>");
+echo $mymovie[0];
+echo $mymovie[1];
+echo $mymovie[2];
+?>
+
+</body>
+</html> 
