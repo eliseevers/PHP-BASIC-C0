ab7f58e92b324b4f53a2407406854113e21f55bf
eliseevers <2155620@talnet.nl>
2021-12-21 13:11:05 +0100

    3.5

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$laag_hoog = array (676, 5345, 78657, 3, 46, 21, 47);
+asort($laag_hoog);
+print_r($laag_hoog);
+$hoog_laag = array (676, 5345, 78657, 3, 46, 21, 47);
+arsort($hoog_laag);
+print_r($hoog_laag);
+
+
+?>
+
+</body>
+</html> 
