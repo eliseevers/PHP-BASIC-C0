18357ce1a791836115a9b3b50ca51b18fb52056e
eliseevers <2155620@talnet.nl>
2021-12-21 13:41:33 +0100

    4.1

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getal1 = 5;
+$getal2 = 10;
+
+echo $getal1 + $getal2;
+
+echo $getal1 / $getal2;
+
+echo $getal1 * $getal2;
+
+echo $getal1 - $getal2;
+?>
+
+</body>
+</html> 
