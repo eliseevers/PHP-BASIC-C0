77ef95905200f7236268d88446835a55943bc9e6
eliseevers <2155620@talnet.nl>
2021-12-22 13:54:26 +0100

    5.1

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$_tekst_1 = 12;
+$_tekst_2 = "gewoon tekst";
+var_dump( $_tekst_1 == $_tekst_1);
+var_dump($_tekst_1 === $_tekst_2);
+var_dump($_tekst_1 != $_tekst_2);
+var_dump($_tekst_1 > $_tekst_2);
+var_dump($_tekst_1 < $_tekst_2);
+var_dump($_tekst_1 <> $_tekst_2);
+var_dump($_tekst_1 >= $_tekst_2);
+var_dump($_tekst_1 <= $_tekst_2);
+
+
+
+?>
+
+</body>
+</html> 
