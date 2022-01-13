3ca34a554fbd184291483c95e58fcd2f4f2eadb1
eliseevers <2155620@talnet.nl>
2022-01-13 11:50:35 +0100

    5.5

---
+++ 5-Conditional_Statements/5.5 - switch conditional/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$score = 20;
+
+switch($score) {
+    case 20:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 19:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 15:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 10:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+?>
+
+</body>
+</html> 
