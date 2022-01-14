02b11f241976a441d9b8d743609bf33537328d4b
eliseevers <2155620@talnet.nl>
2022-01-14 11:27:27 +0100

    6.1

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$woord = "hallo";
+$zin = "mooie blauwe planeet";
+$helezin = "Hallo mooie blauwe planeet";
+function zetTweeWoordenAanElkaar(){
+    echo "hallo mooie blauwe planeet";
+}
+
+zetTweeWoordenAanElkaar();
+?>
+
+</body>
+</html> 
