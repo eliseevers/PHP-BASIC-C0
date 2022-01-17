55cc9da37599e481b2b9d691297aacc4d6f1c6f7
eliseevers <2155620@talnet.nl>
2022-01-17 13:35:31 +0100

    6.3

---
+++ 6-Functions/6.3 - strtoupper()/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function telAlleWoorden(){
+    $mooieblauweauto = "Mijn blauwe auto is een mooie snelle wagen";
+    $lengteRegel = strlen($mooieblauweauto);
+    echo $lengteRegel;
+}
+telAlleWoorden() 
+
+?>
+
+</body>
+</html> 
