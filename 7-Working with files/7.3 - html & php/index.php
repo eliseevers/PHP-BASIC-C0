57025e9309e0781d884b98124679556bd4c8e7f5
eliseevers <2155620@talnet.nl>
2022-01-19 17:47:12 +0100

    7.3

---
+++ 7-Working with files/7.3 - html & php/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+
+    <div id="isMeerderJarig">
+        <?php
+            $leeftijd = 17;
+            if($leeftijd < 18 ){
+                echo "<h6>Je bent te jong om deze pagina te bekijken</h6>";
+            }
+        ?>
+    </div>
+
+</body>
+</html>
