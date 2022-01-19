ba7ca9de018f3ad70592c107fe15215ab5163a1f
eliseevers <2155620@talnet.nl>
2022-01-19 11:48:10 +0100

    menu file 7.1

---
+++ 7-Working with files/7.1 - include/includes/menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$menu1 = "home";
+$menu2 = "blog";
+$menu3 = "contact";
+
+echo "<li>". $menu1 . "</li>" . "<li>" . $menu2 . "</li>" . "<li>". $menu3 . "</li>";
+
+
+?>
+
+</body>
+</html>
